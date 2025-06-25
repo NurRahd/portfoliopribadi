04ac550702cdbf1b19796f6e55092474fce8fd2c
ayam den lapeh <41271248-mbintangalffajr@users.noreply.replit.com>
2025-06-25 16:29:38 +0000

    Restored to 'f7c0d7b2b85ddeb08ee138a147b7164051d69bc3'
    Replit-Restored-To: f7c0d7b2b85ddeb08ee138a147b7164051d69bc3

--- server/index.ts
+++ server/index.ts
@@ -56,9 +56,10 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // Serve the app on configurable port (5000 default for Replit, 3000 for local)
+  // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
-  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+  // It is the only port that is not firewalled.
+  const port = 5000;
   server.listen({
     port,
     host: "0.0.0.0",
